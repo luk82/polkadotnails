5c4da37bab38ee929f020c080585f01886d87d07
LC <lcweb@proton.me>
2024-02-06 10:21:47 +0000

    Add card

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  images: {
+    domains: ["localhost"], // Add 'localhost' to the list if you're running your project locally
+  },
 };
 
 module.exports = nextConfig;
