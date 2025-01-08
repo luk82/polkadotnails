9d7eb521fa1b6840e9f820be7361594ff77e3c86
LC <lukas@lcweb.co.uk>
2025-01-08 17:21:02 +0000

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    domains: ["localhost"], // Add 'localhost' to the list if you're running your project locally
-  },
+  // images: {
+  //   domains: ["localhost"], // Add 'localhost' to the list if you're running your project locally
+  // },
 };
 
 module.exports = nextConfig;
